ae68f81e27d55b4609ede9a9585b50a56d383356
RipperSTL <ripper1028@yahoo.com>
2015-02-01 17:50:27 -0600

    Fixed build path with proper support libriaries.

--- PaintProCalc/src/com/superiorcedarstl/paintprocalc/MainActivity.java
+++ PaintProCalc/src/com/superiorcedarstl/paintprocalc/MainActivity.java
@@ -1,24 +1,23 @@
 package com.superiorcedarstl.paintprocalc;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.abc_action_bar_decor);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+//        getMenuInflater().inflate(R.menu.this , menu);
         return true;
     }
 
@@ -28,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.search_button) {
             return true;
         }
         return super.onOptionsItemSelected(item);
