a1ca836905a3ae36cc6230248f87a2673a436520
RipperSTL <ripper1028@yahoo.com>
2015-02-07 18:24:00 -0600

    Added Room

---
+++ PaintProCalc/src/com/superiorcedarstl/paintprocalc/Room.java
@@ -0,0 +1,32 @@
+package com.superiorcedarstl.paintprocalc;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Room implements Paintable {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isPaintable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public JSONObject toJSON() throws JSONException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
